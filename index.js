b5bde9b27142ec800377a19aa74fe8022b741c4b
kalathurutejaa <kalathuruteja7883@gmail.com>
2023-02-06 15:51:13 +0530

    Txon_task_1

---
+++ index.js
@@ -0,0 +1,74 @@
+<!DOCTYPE html> 
+<html>
+
+<head>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous" />
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+    <script src="https://kit.fontawesome.com/20c5629a29.js" crossorigin="anonymous"></script>
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-primary">
+        <img src='https://res.cloudinary.com/kalathuruteja/image/upload/v1675659411/download_uq3iv4.jpg' width=30px height=30px />
+        <a class="navbar-brand heading " href="#">Txon</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">About</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Jobs</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">InternShips</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+    <div class="bg-container d-flex justify-content-center flex-column">
+        <div class="text-center">
+            <h1 class="banner-heading mb-3">Internship offer letter</h1>
+            <p class="banner-caption mb-4">you need to complete 4 ( four ) tasks in this internship. 1st task you will
+                have to complete in the first week and 2nd task you will have to complete
+                2nd the week and 3rd task you will have to complete in 3rd week and 4th
+                task you will have to complete in 4th week.</p>
+
+            <ol type='none'>
+                <li>Web development<span> and</span> <br />web design internship.</li>
+                <li>Android development.</li>
+                <li>Graphic designer.</li>
+                <li>Machine Learning</li>
+            </ol>
+            <button class="custom-button">Apply</button>
+            <button class="custom-outline-button">Register</button>
+
+            <div class="col-12">
+                <div class="d-flex flex-row justify-content-center">
+                    <div class="follow-us-icon-container">
+                        <i class="fab fa-twitter icon"></i>
+                    </div>
+                    <div class="follow-us-icon-container">
+                        <i class="fab fa-instagram icon"></i>
+                    </div>
+                    <div class="follow-us-icon-container">
+                        <i class="fab fa-facebook icon"></i>
+                    </div>
+                    <div class="follow-us-icon-container">
+                        <i class="fab fa-linkedin icon"></i>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</body>
+
+</html>
